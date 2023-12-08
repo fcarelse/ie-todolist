aa6448a30f859b5f6180b8a06225c1edb553e59a
frank <frank@js.ie>
2023-12-08 11:15:05 +0000

    API filesystem restructure

--- api/src/main/java/ie/todolist/api/Controller/TodolistController.java
+++ api/src/main/java/ie/todolist/api/Controller/TodolistController.java
@@ -1,6 +1,5 @@
 package ie.todolist.api.Controller;
 
-import ie.todolist.api.Model.MessageResponse;
 import ie.todolist.api.Model.Todolist;
 import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
@@ -12,8 +11,8 @@ public class TodolistController {
 
   @GetMapping(value="/rest/todolist", produces = MediaType.APPLICATION_JSON_VALUE)
   @ResponseBody
-  public ResponseEntity<List<Todolist>> restTodolist(){
-    List<Todolist> todolists = new List<Todolist>();
+  public ResponseEntity<List<Todolist>> getTodolist(){
+    List<Todolist> todolists = List.of(new Todolist());
     return ResponseEntity.ok(todolists);
   }
 
